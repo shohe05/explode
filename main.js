9fb6496b0351c5b4a58f1aa819b6538033909e2e
Syouhei Tanaka <shou.tanak@gmail.com>
2014-09-17 17:58:32 +0900

    ほぼおわり

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ enchant();
 
 window.onload = function() {
   var core = new Core(320, 320);
-  core.preload(['chara7.png', 'chara2.png', 'icon0.png', 'monster4.gif', 'effect0.png', 'clear.png', 'map2.png']);
+  core.preload(['chara7.png', 'chara2.png', 'icon0.png', 'monster4.gif', 'effect0.png', 'clear.png', 'map2.png', 'bigmonster1.gif', 'bgm.wav', 'bomb.mp3', 'bigbomb.mp3']);
   core.fps = 10;
   core.onload = function() {
+  var bgm = core.assets['bgm.wav'].clone();
+  var soundBomb = core.assets['bomb.mp3'];
+  var soundBigBomb = core.assets['bigbomb.mp3'];
+  bgm.play();
   gameOverScene = new Scene();
   gameOverScene.backgroundColor = 'black';
   var clear = new Sprite(267, 48);
@@ -14,7 +18,33 @@ window.onload = function() {
   gameClearScene = new Scene();
   gameClearScene.addChild(clear);
 
-    // 自機
+  var map = new Map(16, 16);
+  map.image = core.assets['map2.png'];
+  var mapData = [
+            [ 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18 ],
+            [ 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19 ],
+            [ 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19 ],
+            [ 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19 ],
+            [ 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20 ],
+            [ 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20 ],
+            [ 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21 ],
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+            [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ],
+            [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ],
+            [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+        ];
+    map.loadData(mapData);
+    core.rootScene.addChild(map);
+
     var Player = Class.create(Sprite, {
       initialize: function(x, y) {
         Sprite.call(this, 32, 32);
@@ -29,16 +59,55 @@ window.onload = function() {
       onenterframe: function() {
         if (core.input.right) this.x += 5;
         if (core.input.left) this.x -= 5;
-        if (core.input.up) this.throwBomb();
         for (i = 0; i < enemies.length; i++) {
           if (enemies[i].presence == true && this.intersect(enemies[i])) this.die();
         }
+        var self = this;
+        document.onkeydown = function(e) {
+          if (e.keyCode == 65) {
+            self.throwBomb(1);
+          }
+          if (e.keyCode == 81) {
+            self.throwBomb('up');
+          }
+          if (e.keyCode == 88) {
+            self.throwBomb(3);
+          }
+          if (e.keyCode == 83) {
+            self.throwBigBomb(1);
+          }
+          if (e.keyCode == 67) {
+            self.throwBigBomb(3);
+          }
+        };
+
       },
 
-      throwBomb: function() {
+      throwBomb: function(power) {
+        if (power == 'up') {
+          this.tl.cue({
+            0: function() { bomb = new Bomb(this.x+3, this.y-20); },
+            2: function() { bomb.tl.moveTo(this.x, this.y - 40, 3); },
+            3: function() { bomb.tl.moveTo(this.x, this.y - 70, 2); },
+            4: function() { bomb.tl.moveTo(this.x, this.y-30, 2); },
+            4: function() { bomb.tl.moveTo(this.x, this.y+20, 1); }
+          })
+        } else {
+          this.tl.cue({
+            0: function() { bomb = new Bomb(this.x+3, this.y); },
+            2: function() { bomb.tl.moveTo(this.x + 30 * power, this.y - 15 * power, 2); },
+            3: function() { bomb.tl.moveTo(this.x + 50 * power, this.y - 5 * power, 1); },
+            4: function() { bomb.tl.moveTo(this.x + 60 * power, this.y + 15, 0.1); }
+          })
+        }
+      },
+
+      throwBigBomb: function(power) {
         this.tl.cue({
-          0: function() { bomb = new Bomb(this.x+3, this.y+10); },
-          2: function() { bomb.tl.moveBy(this.x+1, this.y, 10); }
+          0: function() { bomb = new BigBomb(this.x+3, this.y); },
+          2: function() { bomb.tl.moveTo(this.x + 30 * power, this.y - 15 * power, 2); },
+          3: function() { bomb.tl.moveTo(this.x + 50 * power, this.y - 5 * power, 1); },
+          4: function() { bomb.tl.moveTo(this.x + 60 * power, this.y + 15, 0.1); }
         })
       },
 
@@ -68,12 +137,51 @@ window.onload = function() {
         this.x -= 4;
       },
 
-      damage: function() {
-        this.life --;
-        if (this.presence == true && this.life == 0) this.die();
+      damage: function(power) {
+        this.life -= power;
+        if (this.presence == true && this.life <= 0) this.die();
       },
 
       die: function() {
+        this.tl.removeFromScene();
+        this.presence = false;
+      }
+    });
+
+    // 敵機
+    var Boss = Class.create(Sprite, {
+      initialize: function(x, y) {
+        Sprite.call(this, 80, 80);
+        this.x = x;
+        this.y = y;
+        this.frame = 2;
+        this.image = core.assets['bigmonster1.gif'];
+        this.life = 20;
+        this.presence = true;
+
+        core.rootScene.addChild(this);
+        this.tl.moveBy(20, 20, 5)
+          .moveBy(-20, -20, 5)
+          .loop();
+      },
+
+      onenterframe: function() {
+        if (this.frame = 10) this.frame = 2;
+        this.frame ++;
+        this.x -= 4;
+      },
+
+      damage: function(power) {
+        this.life -= power;
+        var frame = this.frame;
+        this.frame = 7;
+        setTimeout(function(){ this.frame = frame; }, 1000);
+        if (this.presence == true && this.life <= 0) this.die();
+      },
+
+      die: function() {
+        this.frame = 1
+        setTimeout(function(){ this.frame = 0; }, 1000);
         this.tl.removeFromScene();
         this.presence = false;
       }
@@ -93,10 +201,16 @@ window.onload = function() {
       },
 
       onenterframe: function() {
+        if (this.y >= 240) {
+          this.explode();
+        }
         for (i = 0; i < enemies.length; i++) {
           if (this.presence == true && enemies[i].presence == true && this.intersect(enemies[i])) {
             this.explode();
-            enemies[i].damage();
+            enemies[i].damage(1);
+          }
+          if (this.presence == true && this.intersect(player)) {
+            player.die();
           }
         }
       },
@@ -104,8 +218,53 @@ window.onload = function() {
       explode: function() {
         this.image = core.assets['effect0.png'];
         this.tl.cue({
-          0: function() { for (i=0; i<4; i++) this.frame = i; },
+          0: function() { for (i=0; i<4; i++) this.frame = i; soundBomb.play();},
           3: function() { core.rootScene.removeChild(this); this.presence = false;}
+        })
+      }
+
+    });
+
+    // 爆弾
+    var BigBomb = Class.create(Sprite, {
+      initialize: function(x, y) {
+        Sprite.call(this, 16, 16);
+        this.x = x;
+        this.y = y;
+        this.frame = 24;
+        this.image = core.assets['icon0.png'];
+        this.presence = true;
+        this.scale(3, 3);
+
+        core.rootScene.addChild(this);
+      },
+
+      onenterframe: function() {
+        var self = this;
+        if (this.y >= 226) {
+          setTimeout(function(){self.explode();}, 3000);
+        }
+      },
+
+      explode: function() {
+        this.image = core.assets['effect0.png'];
+        soundBigBomb.play();
+        this.scale(2.5, 2.5);
+        this.tl.cue({
+          0: function() {
+            for (i=0; i<4; i++) {
+              this.frame = i;
+              for (i = 0; i < enemies.length; i++) {
+                if (this.presence == true && enemies[i].presence == true && this.within(enemies[i], 90)) {
+                  enemies[i].damage(3);
+                }
+                if (this.presence == true && this.within(player, 90)) {
+                  player.die();
+                }
+              }
+            }
+          },
+          3: function() { core.rootScene.removeChild(this); this.presence = false; this.image = core.assets['icon0.png'];}
         })
       }
 
@@ -134,16 +293,13 @@ window.onload = function() {
 
     });
 
-    var player = new Player(100, 10);
-    var goal = new Goal(280, 10);
+    var player = new Player(100, 229);
+    var goal = new Goal(280, 236);
     var enemies = [];
-    for (i = 0; i < 20; i++) {
-      enemies[i] = new Enemy(200+(30*i), 10);
+    for (i = 0; i < 50; i++) {
+      enemies[i] = new Enemy(200+(30*i), 224);
     }
+    enemies.push(new Boss(400, 150));
   }
   core.start();
 };
-
-function rand(n) {
-  return Math.floor(Math.random() * (n+1));
-}
